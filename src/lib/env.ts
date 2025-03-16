4df2b6e3a9f6d5bb674a45e80df6411045350dab
Samuel Wondimu <54416265+samuelwondimu@users.noreply.github.com>
2025-03-17 01:35:46 +0300

    updated new pages

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,8 @@
-import errors from "./errors"
-
 export const dev = process.env.NODE_ENV === 'development'
 export const vercelPreview = process.env.VERCEL_ENV === 'preview'
 
 if (!process.env.NEXT_PUBLIC_BASE_URL) {
-	throw new Error(errors.missingBaseUrl)
+	throw new Error('Missing base url: 🤞 Domain Expansion (Unlimited Void) requires a proper URL!\n\n')
 }
 
 export const BASE_URL = dev
